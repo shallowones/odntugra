911ea1653af0a989cf22f1435ec4dd5824592097
bpa <bpa@ugraweb.ru>
2017-08-31 15:45:39 +0500

    Исправлен баг с добавлением символьных кодов для нвоостей

--- local/php_interface/inc/newsHandler.php
+++ local/php_interface/inc/newsHandler.php
@@ -1,17 +1,22 @@
 <? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
-AddEventHandler('iblock', 'OnBeforeIBlockElementAdd', ['CodeAdder', 'OnBeforeIBlockElementAddHandler']);
-AddEventHandler('iblock', 'OnBeforeIBlockElementUpdate', ['CodeAdder', 'OnBeforeIBlockElementAddHandler']);
+AddEventHandler('iblock', 'OnAfterIBlockElementAdd', ['CodeAdder', 'OnAfterIBlockElementAddHandler']);
 
 class CodeAdder
 {
-    function OnBeforeIBlockElementAddHandler(&$arFields)
+    function OnAfterIBlockElementAddHandler(&$arFields)
     {
         if (intval($arFields['IBLOCK_ID']) !== intval(\UW\IBHelper::getIbId(\UW\IBCodes::IB_CODE_NEWS))
             && !empty($arFields['CODE']))
             return true;
 
         $date = new \DateTime($arFields['ACTIVE_FROM']);
-        $arFields['CODE'] = $arFields['ID'] . '-' . $date->format('dmy');
+        $el = new CIBlockElement;
+        $el->Update(
+            $arFields['ID'],
+            [
+                'CODE' => $arFields['ID'] . '-' . $date->format('dmy')
+            ]
+        );
 
         return true;
     }
