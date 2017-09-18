1e2d6a904034690be7ee26143cb80b90f7dc8529
Anton <shulan95@gmail.com>
2017-09-18 10:39:50 +0500

    Добавление переменной в компаненты и изменение стилей

--- local/components/uw/handbook/templates/.default/template.php
+++ local/components/uw/handbook/templates/.default/template.php
@@ -1,4 +1,5 @@
 <? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+/** @var array $arResult */
 ?>
     <div class="hide-block">
         <? foreach ($arResult['section'] as $mo): ?>
--- local/templates/print/components/bitrix/news/news/bitrix/news.detail/.default/template.php
+++ local/templates/print/components/bitrix/news/news/bitrix/news.detail/.default/template.php
@@ -1,6 +1,6 @@
 <? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 $this->setFrameMode(true);
-
+/** @var array $arResult */
 $fileValue = $arResult['DISPLAY_PROPERTIES']['FILES']['FILE_VALUE'];
 $photoValue = $arResult['DISPLAY_PROPERTIES']['PHOTOS']['FILE_VALUE'];
 ?>
