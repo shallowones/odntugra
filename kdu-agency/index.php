d71fe55e89b7484f3e78f17f43015655de095242
Your Name <you@example.com>
2017-09-21 09:15:44 +0500

    Изменения с дева 21.09.2017

--- kdu-agency/index.php
+++ kdu-agency/index.php
@@ -1,20 +1,14 @@
 <? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 /** @global CMain $APPLICATION */
 $APPLICATION->SetTitle("Учреждения КДУ муниципальных образований Югры");
-?>
-
-<? $APPLICATION->IncludeComponent("uw:inst", ".default", Array(
-    "IBLOCK_ID" => \UW\IBHelper::getIbId(\UW\IBCodes::IB_CODE_AGENCY),    // Идентификатор инфоблока
-    "SEF_FOLDER" => "/kdu-agency/",    // Каталог ЧПУ (относительно корня сайта)
-    "SEF_MODE" => "Y",    // Включить поддержку ЧПУ
-    "COMPONENT_TEMPLATE" => ".default",
-    "ELEM_COUNT" => "5",    // Количество элементов на странице
-    "SEF_URL_TEMPLATES" => array(
-        "list" => "",
-        "detail" => "#ELEMENT_CODE#/",
-    )
-),
-    false
-); ?>
-
-<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
+?><?$APPLICATION->IncludeComponent(
+	"uw:inst",
+	"",
+	Array(
+		"ELEM_COUNT" => "5",
+		"IBLOCK_ID" => \UW\IBHelper::getIbId(\UW\IBCodes::IB_CODE_AGENCY),
+		"SEF_FOLDER" => "/kdu-agency/",
+		"SEF_MODE" => "Y",
+		"SEF_URL_TEMPLATES" => Array("detail"=>"#ELEMENT_CODE#/","list"=>"")
+	)
+);?><? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
--- kdu-agency/kamerton/index.php
+++ kdu-agency/kamerton/index.php
@@ -1,12 +1,14 @@
-<?
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("МАУК Белоярского района \"Центр культуры и досуга \"Камертон\"");
-?><img width="181" alt="e3eb399e159c4906e63ea05d66c02364.jpg" src="/upload/medialibrary/1dc/1dc291888952db733f217eb57def27da.jpg" height="300" title="e3eb399e159c4906e63ea05d66c02364.jpg" align="left" style="margin-right: 10px;">Директор:&nbsp;Авилова&nbsp;Юлия Николаевна<br>
- <br>
- Адрес:&nbsp;г. Магадан, Переулок Школьный, д. 3<br>
- <br>
- Тел.&nbsp;(8-413-2) 63-08-25<br>
- <br>
- E-mail:&nbsp;m<a href="mailto:mag.otok@mail.ru">ag.otok@mail.ru</a><br>
- <br>
- Сайт:&nbsp;<a href="http://www.rusfolk.ru/www.gauk-otok.ru">www.gauk-otok.ru</a><br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+/** @global CMain $APPLICATION */
+$APPLICATION->SetTitle("Учреждения КДУ муниципальных образований Югры");
+?><?$APPLICATION->IncludeComponent(
+	"uw:inst",
+	"",
+	Array(
+		"ELEM_COUNT" => "5",
+		"IBLOCK_ID" => \UW\IBHelper::getIbId(\UW\IBCodes::IB_CODE_AGENCY),
+		"SEF_FOLDER" => "/kdu-agency/",
+		"SEF_MODE" => "Y",
+		"SEF_URL_TEMPLATES" => Array("detail"=>"#ELEMENT_CODE#/","list"=>"")
+	)
+);?><? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
